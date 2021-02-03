30c13e5b759ae0ed7646c38224cadd6ed28a6ab9
AnandLakabshetti <AnandLakabshetti@gmail.com>
2021-02-03 23:08:33 +0530

    adding a variable.py in chapter_2

---
+++ chapter_2/variable.py
@@ -0,0 +1,14 @@
+
+#we should not use the reserved keywords for varible creations
+
+name= "Anand"
+charactar='c'
+integer=10
+float_f=22.22
+bolean =True    
+print (bolean)
+print (type (bolean))
+#printing the variable 
+print ("name-",name,"character-",charactar,"integer-",integer,"float-",float_f)
+#printing the type of variable 
+print (type( name),type (charactar), type (integer),type (float_f))
